ba20fc67c497f9299a298b8f9b6ef90a63543e8e
Toshiven <kevin.abler19@gmail.com>
2023-08-03 14:48:52 +0800

    final commit(no design yet)

--- addition.js
+++ addition.js
@@ -3,49 +3,63 @@ const input1 = document.getElementById("num2");
 const output = document.getElementById("output");
 const optionSelector = document.getElementById("ops");
 
-//event listener to know if selection option changed
-optionSelector.addEventListener("change", () =>{
-    //getting optionSelector value into selectedOption for switch to know what is selected
-    const selectedOption = optionSelector.value;
+const addition = () => {
+    //parses input value to float and does the maths
+    const num1 = parseFloat(input0.value);
+    const num2 = parseFloat(input1.value);
+    const answer = parseFloat(num1) + parseFloat(num2);
+    return answer;
+}
 
-    //
-    switch(selectedOption){
-        case 'add':
-            //on click of solve button will call addition() function
-            document.getElementById("solve").addEventListener("click", function () {
-                const solved = addition();
-                //outputs value of addition() function
-                output.value = solved;
-                console.log("it works");
-            })
-            
-            const addition = () => {
-                //parses input value to float and does addition
-                const num1 = parseFloat(input0.value);
-                const num2 = parseFloat(input1.value);
-                const answer = parseFloat(num1) + parseFloat(num2);
-                return answer;
-            }
-            break;
-        case 'minus':
-            //on click of solve button will call subtraction() function
-            document.getElementById("solve").addEventListener("click", function () {
-                //outputs value of subtraction() function
-                const solved = subtraction();
-                output.value = solved;
-                console.log("it works");
-            })
+const subtraction = () => {
+    //parses input value to float and does the maths
+    const num1 = parseFloat(input0.value);
+    const num2 = parseFloat(input1.value);
+    const answer = parseFloat(num1) - parseFloat(num2);
+    return answer;
+}
 
-            const subtraction = () => {
-                //parses input value to float and does addition
-                const num1 = parseFloat(input0.value);
-                const num2 = parseFloat(input1.value);
-                const answer = parseFloat(num1) - parseFloat(num2);
-                return answer;
-            }
-            break;
+const multiplication = () => {
+    const num1 = parseFloat(input0.value);
+    const num2 = parseFloat(input1.value);
+    const answer = num1 * num2;
+    return answer;
+}
 
+const division = () => {
+    const num1 = parseFloat(input0.value);
+    const num2 = parseFloat(input1.value);
+    const answer = num1 / num2;
+    return answer;
+}
+
+    const button = () => {
+            //getting optionSelector value into selectedOption for switch to know what is selected
+        const selectedOption = optionSelector.value;
+        switch(selectedOption){
+            case 'add':
+                    const solvedA = addition();
+                    //outputs value of addition() function
+                    output.value = solvedA;
+                    console.log("it works");
+                break;
+            case 'minus':
+                    const solvedS = subtraction();
+                    //outputs value of subtraction() function
+                    output.value = solvedS;
+                    console.log("it works");
+                break;
+            case 'multiply':
+                    const solvedM = multiplication();
+                    output.value = solvedM;
+                    console.log("it worksss");
+                break;
+            case 'divide':
+                    const solvedD = division();
+                    output.value = solvedD;
+                break;
+        }
     }
-})
-console.log(addition());
-document.getElementById("output").value = addition();
+
+    //adds a event listener click to solve all your problems ;3
+    document.getElementById("solve").addEventListener("click", button);
